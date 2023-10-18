6c7189f7c86dfbcebd5e19c8c48160b2602a1c62
xyz <pappukumargupta1999@gmail.com>
2023-10-18 21:42:30 +0530

    project commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+buttons.forEach((button) => {
+  console.log(button);
+  button.addEventListener('click', (e) => {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.background = 'grey';
+    }
+    if (e.target.id === 'white') {
+      body.style.background = 'white';
+    }
+    if (e.target.id === 'blue') {
+      body.style.background = 'blue';
+    }
+    if (e.target.id === 'yellow') {
+      body.style.background = 'yellow';
+    }
+  });
+});
